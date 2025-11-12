fbd5ab2b267a86e44c6f51dc654e0378e115f175
aiya <aichkaxxx2007@gmail.com>
2025-11-12 17:36:46 +0500

    deleted comments

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,12 @@ public class Main {
 
         IProductBuilder builder = new ComputerBuilder();
 
-        // Устанавливаем компоненты
         Computer pc = builder.setCPU(new CPU("Intel Core i7-13700K", 170000, 8, 32))
             .setRAM(new RAM("Kingston Fury 32GB DDR5", 45000, 16))
             .setGPU(new GPU("NVIDIA RTX 4070", 350000, 8))
             .setStorage(new Storage("Samsung 990 Pro 1TB SSD", 65000, "SSD", 512))
             .getComputer();
 
-        // Выводим результат
         System.out.println(pc);
 
 
