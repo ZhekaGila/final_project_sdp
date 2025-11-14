38a64eab73dbca9505ad95b73e8dea28d0d8b284
ZhekaGila <gilazhevzhandos@gmail.com>
2025-11-14 18:05:56 +0500

    refactoring text

--- src/builder/product/components/GPU.java
+++ src/builder/product/components/GPU.java
@@ -3,6 +3,8 @@ package builder.product.components;
 public class GPU extends Component {
     private int vram;
 
+    private String descriptionText = "GPU: " + model + " (" + vram + " GB VRAM)";
+
     public GPU(String model, int vram, int price) {
         super(model, price);
         this.vram = vram;
@@ -10,6 +12,6 @@ public class GPU extends Component {
 
     @Override
     public String getDescription() {
-        return "GPU: " + model + " (" + vram + " GB VRAM)";
+        return descriptionText;
     }
 }
--- src/factory/concrete/PayPalPayment.java
+++ src/factory/concrete/PayPalPayment.java
@@ -5,14 +5,17 @@ import model.User;
 
 public class PayPalPayment implements IPayment {
 
+    private String paymentSuccessMessage = "Payment process was succeed";
+    private String paymentErrorMessage = "Not enough balance. Payment process was not succeed";
+
     @Override
     public void processPayment(User user, float price) {
         float balance = user.getWallet().getBalance();
         if (balance < price) {
-            System.out.println("Not enough balance. Payment process was not succeed");
+            System.out.println(paymentErrorMessage);
         } else{
             user.getWallet().setBalance(balance - price);
-            System.out.println("Payment process was succeed");
+            System.out.println(paymentSuccessMessage);
         }
     }
 }
